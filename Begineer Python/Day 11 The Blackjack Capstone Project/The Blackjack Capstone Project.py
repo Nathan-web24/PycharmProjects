20522d39202b4f5e6bb6751aa1706cde04795464
Nathan-web24 <78878767+Nathan-web24@users.noreply.github.com>
2022-09-06 17:18:29 +1200

    Finish

--- Begineer Python/Day 11 The Blackjack Capstone Project/The Blackjack Capstone Project.py
+++ Begineer Python/Day 11 The Blackjack Capstone Project/The Blackjack Capstone Project.py
@@ -33,9 +33,11 @@
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 import random
-from replit import clear
+import os
 from art import logo
 
+def clear():
+  os.system('clear')
 def deal_card():
   """Returns a random card from the deck."""
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
